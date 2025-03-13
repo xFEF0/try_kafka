4bae63bae07fd162cf14711415689732ddda8731
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-03-13 17:06:22 -0300

    initial commit

---
+++ src/main/java/com/xfef0/try_kafka/TryKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.xfef0.try_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xfef0/try_kafka/TryKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfef0.try_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
