d5c399e57c3c46ff044010d4567c4be94ec51cc7
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-03-16 17:10:55 -0300

    add consumer

---
+++ src/main/java/com/xfef0/try_kafka/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.xfef0.try_kafka.kafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test_topic", groupId = "group_id")
+    public void consume(String message) {
+        log.info("Received: {}", message);
+    }
+}
