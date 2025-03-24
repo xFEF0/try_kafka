b3275600f1d697d2f7991eb4ce7c8716429413d4
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-03-24 16:43:03 -0300

    add producer module to publish wikimedia recent changes

---
+++ kafka-producer-wiki/src/main/java/com/xfef0/springboot/SpringBootProducerApplication.java
@@ -0,0 +1,12 @@
+package com.xfef0.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProducerApplication.class);
+    }
+}
