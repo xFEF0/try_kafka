99189bc0fbf0241786b5f6d9ce7bd294bc9b2967
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-03-24 18:02:06 -0300

    add consumer module

---
+++ kafka-consumer-wiki/src/main/java/com/xfef0/springboot/SpringBootConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.xfef0.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsumerApplication.class);
+    }
+
+}
